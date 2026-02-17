afcb03d215080a67a95f470d84942aa4068a40b9
Kazuki Hamada <34650567+kzkhmd@users.noreply.github.com>
2026-02-17 19:15:30 +0900

    テンプレートファイルを追加

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -11,3 +11,12 @@ def index():
            endpoint='hello-endpoint')
 def hello(name):
     return f"Hello, {name}!"
+
+@app.route('/name/<name>')
+def show_name(name):
+    return render_template('index.html', name=name)
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('hello-endpoint', name='world'))
+    print(url_for('show_name', name='Ichiro', page='1'))
