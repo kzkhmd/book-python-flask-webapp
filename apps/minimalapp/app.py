c171cd3a552bc8927473d4ad5b8129f0cc65efd0
Kazuki Hamada <34650567+kzkhmd@users.noreply.github.com>
2026-02-17 18:53:53 +0900

    helloメソッドの実装

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello, Minimal App!"
+
+@app.route('/hello/<name>',
+           methods=['GET'],
+           endpoint='hello-endpoint')
+def hello(name):
+    return f"Hello, {name}!"
