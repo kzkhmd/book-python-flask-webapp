bc5976e341c0b0158f5e64f6ea8678b65ffe2e62
Kazuki Hamada <34650567+kzkhmd@users.noreply.github.com>
2026-02-24 18:39:55 +0900

    問い合わせフォーム入力後にメールを送信する処理を追加

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,3 +1,5 @@
+import logging
+import os
 from email_validator import validate_email, EmailNotValidError
 from flask import (
     Flask,
@@ -6,11 +8,29 @@ from flask import (
     request,
     redirect,
     flash,
+    make_response,
+    session
 )
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
+app.debug = True
 app.config["SECRET_KEY"] = "2AZSMss3p5QPbcY2hBsJ"
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER")
+app.config["MAIL_PORT"] = os.environ.get("MAIL_PORT")
+app.config["MAIL_USE_TLS"] = os.environ.get("MAIL_USE_TLS")
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER")
+
+mail = Mail(app)
+
+app.logger.setLevel("DEBUG")
+
+toolbar = DebugToolbarExtension(app)
 
 @app.route('/')
 def index():
@@ -28,7 +48,10 @@ def show_name(name):
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    response = make_response(render_template('contact.html'))
+    response.set_cookie('flaskbook key', 'flaskbook value')
+    session['username'] = 'flaskbook'
+    return response
 
 @app.route('/contact/complete', methods=['GET', 'POST'])
 def contact_complete():
@@ -59,10 +82,24 @@ def contact_complete():
         if not is_valid:
             return redirect(url_for('contact'))
         
+        send_mail(
+            email,
+            "お問い合わせありがとうございます",
+            "contact_mail",
+            username=username,
+            description=description
+        )
+        
         flash("お問い合わせありがとうございました！")
         return redirect(url_for('contact_complete'))
     
     return render_template('contact_complete.html')
+
+def send_mail(to, subject, template, **kwargs):
+    msg = Message(subject, recipients=[to])
+    msg.body = render_template(f"{template}.txt", **kwargs)
+    msg.html = render_template(f"{template}.html", **kwargs)
+    mail.send(msg)
 
 with app.test_request_context():
     print(url_for('index'))
