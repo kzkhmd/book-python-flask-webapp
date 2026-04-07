8123e2106c6341b2f4aac7e6cba2138a2cfb73a7
Kazuki Hamada <34650567+kzkhmd@users.noreply.github.com>
2026-04-07 19:09:46 +0900

    物体検知アプリの登録

--- apps/app.py
+++ apps/app.py
@@ -25,8 +25,10 @@ def create_app(config_key):
 
     from apps.crud import views as crud_views
     from apps.auth import views as auth_views
+    from apps.detector import views as dt_views
 
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
     app.register_blueprint(auth_views.auth, url_prefix="/auth")
+    app.register_blueprint(dt_views.dt)
 
     return app
